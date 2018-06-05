da0f3f1a6de313f05fe16bd9ee8bd348bade0d0f
Kevin <omallek@gmail.com>
2018-06-05 18:38:05 -0500

    updates css for navbar in smaller views

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -21,37 +21,37 @@ export default class Navbar extends Component {
     return (
       <Menu id="navbar" pointing>
         <Link to="/home">
-          <Menu.Item name="home" active={activeItem === 'home'} onClick={this.handleItemClick}>
+          <Menu.Item id="menu-item" name="home" active={activeItem === 'home'} onClick={this.handleItemClick}>
             Home
           </Menu.Item>
         </Link>
         <Link to="/newBook">
-          <Menu.Item name="addBook" active={activeItem === 'addBook'} onClick={this.handleItemClick}>
+          <Menu.Item id="menu-item" name="addBook" active={activeItem === 'addBook'} onClick={this.handleItemClick}>
             Add Book
           </Menu.Item>
         </Link>
         <Link to="/allBooks">
-          <Menu.Item name="allBooks"  active={activeItem === 'allBooks'} onClick={this.handleItemClick}>
+          <Menu.Item id="menu-item" name="allBooks"  active={activeItem === 'allBooks'} onClick={this.handleItemClick}>
             My Books
           </Menu.Item>
         </Link>
         <Link to="/allAuthors">
-          <Menu.Item name="allAuthors" active={activeItem === 'allAuthors'} onClick={this.handleItemClick}>
+          <Menu.Item id="menu-item" name="allAuthors" active={activeItem === 'allAuthors'} onClick={this.handleItemClick}>
             My Authors
           </Menu.Item>
         </Link>
         <Link to="/allGenres">
-          <Menu.Item name="allGenres" active={activeItem === 'allGenres'} onClick={this.handleItemClick}>
+          <Menu.Item id="menu-item" name="allGenres" active={activeItem === 'allGenres'} onClick={this.handleItemClick}>
             My Genres
           </Menu.Item>
         </Link>
         <Link to="/otherUsers">
-          <Menu.Item name="otherUsers" active={activeItem === 'otherUsers'} onClick={this.handleItemClick}>
+          <Menu.Item id="menu-item" name="otherUsers" active={activeItem === 'otherUsers'} onClick={this.handleItemClick}>
             My Friend's Lists
           </Menu.Item>
         </Link>
         <Link to="/funFacts">
-          <Menu.Item name="funFacts" active={activeItem === 'funFacts'} onClick={this.handleItemClick}>
+          <Menu.Item id="menu-item" name="funFacts" active={activeItem === 'funFacts'} onClick={this.handleItemClick}>
             Fun Facts
           </Menu.Item>
         </Link>
