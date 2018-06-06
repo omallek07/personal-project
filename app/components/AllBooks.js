8daea7d3da15588a7a1a3d8a8907ddf6dacedb2e
Kevin <omallek@gmail.com>
2018-06-05 22:17:35 -0500

    fixes all books css responsiveness

--- app/components/AllBooks.js
+++ app/components/AllBooks.js
@@ -18,44 +18,42 @@ class allBooks extends Component {
     let {books} = this.props;
 
     return (
-      <Segment>
-        <Card.Group itemsPerRow={7} stackable>
-          {
-            books.map(book => {
-              return (
-                <Popup
-                key={book.id}
-                trigger={
-                  <Card id="all-books-card" raised>
-                    <Link to={`/books/${book.id}`}>
-                      <Image id="all-books-card-image" fluid size="medium" src={book.coverImage} />
-                    </Link>
-                  </Card>
-                }
-                hoverable
-                size="tiny"
-                >
-                  <Popup.Header>
-                      {book.title}
-                    </Popup.Header>
-                  <Popup.Content>
-                    <Rating icon="star" defaultRating={book.rating} maxRating={5} size="small" disabled />
-                    <Icon className="deleteIcon" name="delete" onClick={() => this.deleteBookHandler(book.id)} size="small" circular corner />
-                </Popup.Content>
-                </Popup>
-              )
-            })
-          }
-          <Card id="add-new-book-card" raised>
-            <Grid verticalAlign="middle" centered>
-              <Grid.Row id="add-new-book-plus" stretched>
-                <Link to="/newBook">
-                  <Icon name="plus" link size="huge" centered />
-                </Link>
-              </Grid.Row>
-            </Grid>
-          </Card>
-        </Card.Group>
+      <Segment id="all-books-segment">
+        {
+          books.map(book => {
+            return (
+              <Popup
+              key={book.id}
+              trigger={
+                <Card id="all-books-card" raised>
+                  <Link to={`/books/${book.id}`}>
+                    <Image id="all-books-card-image" fluid size="medium" src={book.coverImage} />
+                  </Link>
+                </Card>
+              }
+              hoverable
+              size="tiny"
+              >
+                <Popup.Header>
+                    {book.title}
+                  </Popup.Header>
+                <Popup.Content>
+                  <Rating icon="star" defaultRating={book.rating} maxRating={5} size="small" disabled />
+                  <Icon className="deleteIcon" name="delete" onClick={() => this.deleteBookHandler(book.id)} size="small" circular corner />
+              </Popup.Content>
+              </Popup>
+            )
+          })
+        }
+        <Card id="add-new-book-card" raised>
+          <Grid verticalAlign="middle" centered>
+            <Grid.Row id="add-new-book-plus" stretched>
+              <Link to="/newBook">
+                <Icon name="plus" link size="huge" centered />
+              </Link>
+            </Grid.Row>
+          </Grid>
+        </Card>
       </Segment>
     )
   }
